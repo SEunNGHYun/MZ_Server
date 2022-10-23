83e6afaa987a6d1ebb6cd03efc5eeb831be16ecb
ysh12274856@gmail.com <ysh12274856@gmail.com>
2022-10-23 20:44:27 +0900

    scrab api 수정

---
+++ controllers/scrab/delete.js
@@ -0,0 +1,34 @@
+const { getConnection } = require('../../dbConnect')
+
+module.exports = async (req, res) => {
+  try{
+      const dbConnect = await getConnection()
+      const { policy_id } = req.params
+      const { user_id } = req //token복호화하여 앞에 저장해놓은 값을 꺼내기 
+
+
+      const dataCheck = await dbConnect.query("select user_ids, policy_ids from SCRAB_POLICIES_USERS where user_ids = ? and policy_ids = ?", [user_id, policy_id])
+
+      if (dataCheck.length < 0) {
+          return res.status(201).json({
+            status : 201,
+            message : "이미 삭제 되어있습니다."
+          })
+      }
+
+      await dbConnect.query('delete from SCRAB_POLICIES_USERS where user_ids = ? and policy_ids = ?', [user_id, policy_id])
+      
+      dbConnect.query("delete from SCRAB_POLICIES where policy_id = ?" , [policy_id]).then(_ =>{
+        return res.status(202).json({
+          status : 202,
+          message : "삭제 되었습니다."
+        })
+      })
+  
+    }catch(err){
+      return res.status(500).json({
+        status : 500,
+        message : err
+      })
+  }
+}
--- routes/scrab.js
+++ routes/scrab.js
@@ -2,7 +2,12 @@ const routes = require("express").Router();
 
 const tokenCheck = require('./tokenChecks/access_tokenCheck')
 const toggle = require("../controllers/scrab/toggle");
+const add = require('../controllers/scrab/add')
+const del = require('../controllers/scrab/delete')
 
-routes.post('/', tokenCheck, toggle);
+
+routes.post('/', tokenCheck, add);
+routes.delete('/', tokenCheck, del);
+
 
 module.exports = routes;
