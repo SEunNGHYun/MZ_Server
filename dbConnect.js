f4672b779708452c2c90cc1bf79e21f807e9f5f1
ysh12274856@gmail.com <ysh12274856@gmail.com>
2022-10-29 23:46:05 +0900

    connection timeout try2

--- dbConnect.js
+++ dbConnect.js
@@ -10,11 +10,6 @@ const conn = mariadb.createConnection({
   connectionLimit: process.env.DATABASE_LIMIT || 5,
   database: process.env.DATABASE
 })
-//database연결 설정(사용할 db선택, 연결제안, 사용자, db연결 호스트,포트 설정 ...)
-exports.getConnection = () => {
-  return conn.getConnection((err) => {
-    if (err) throw err;
-    console.log("success MariaDB Connect")
-  })
-}
+
+return conn
 
