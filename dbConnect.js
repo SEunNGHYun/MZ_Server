9fbe4f36dccd091dab4095dd04f624fdbaaee874
ysh12274856@gmail.com <ysh12274856@gmail.com>
2022-10-29 23:39:38 +0900

    connection timeout

--- dbConnect.js
+++ dbConnect.js
@@ -3,7 +3,7 @@ const env = require('dotenv')
 
 env.config({ path: './.env' })
 
-const conn = mariadb.createPool({
+const conn = mariadb.createConnection({
   host: process.env.DATABASE_HOST || 'localhost', 
   user: process.env.DATABASE_USER || 'root', 
   password: process.env.DATABASE_PASSWORD,
